77714677bb48a7ba33d01648c90e90ae999efabc
Akash Kumar <akashpk1997@gmail.com>
2020-02-10 11:49:15 +0530

    Added ejs for template rendering

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     var today = new Date();
-
-    if(today.getDay() == 1){
-        res.send("Its the weekend!");
-    } else {
-        res.send("Ohh no! I have work")
-    }
+    var currentDay = today.getDay();
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    res.render('list', {kindofDay: day[currentDay]});
 });
 
 app.post("/", (req, res) => {
