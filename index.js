0145e85fe9aee68e52b6caaee6446daa5a3f3fe4
Akash Kumar <akashpk1997@gmail.com>
2020-02-10 12:56:17 +0530

    Added listing

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+var items = ["First item", "Second item", "Third item"];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    res.render('list', {kindofDay: day[currentDay]});
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    var day = today.toLocaleDateString('en-IN', options);
+
+    res.render('list', {kindofDay: day, newListItems: items});
 });
 
 app.post("/", (req, res) => {
-
+    var newItem = req.body.newItem;
+    items.push(newItem);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
